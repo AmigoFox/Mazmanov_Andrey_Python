bbe1def51a597c5ab7f0ebe777e41bcd29b3b17f
AmigoFox <prostoggwpandre@gmail.com>
2024-10-19 17:19:01 +0300

    Update PZ_3_2.py

--- PZ_3_1/PZ_3_2.py
+++ PZ_3_1/PZ_3_2.py
@@ -1,12 +1,8 @@
-# Даны два числа. Вывести вначале большее, а затем меньшее из
-#них.
+# Даны 2 числа. Вывести вначале большее, затем меньшее
 def exercise_one():
  num1=int(input("input number 1 \n"))
  num2=int(input("input number 2 \n"))
- num3=int(input("input number 3 \n"))
- if (num1==-num2 or num2==-num1 or num3==-num1 or
-num3==-num2):
-    print("Try repeat")
+ if(num1>num2):
+ print("greater number 1:",num1)
  else:
-    print("No repepat")
-exercise_one()
+ print("greater number 2:",num2)
