a7c59a83c74f0460a44548d5cd6497c065995e18
AmigoFox <prostoggwpandre@gmail.com>
2024-10-19 16:49:24 +0300

    Update PZ_2_1.py

--- PZ_2/PZ_2_1.py
+++ PZ_2/PZ_2_1.py
@@ -1,1 +1,8 @@
-print("Hello")
+# Вариант20. Дано 3-х значное число. Вывести число полученное при 
+перестановке цифр исходного числа
+number = int(input("inpyt num "))
+hundreds = number // 100
+tens = (number % 100) // 10
+units = number % 10
+new_number = tens * 100 + hundreds * 10 + units
+print("Number with rearranged hundreds digits and base:", new_number)
