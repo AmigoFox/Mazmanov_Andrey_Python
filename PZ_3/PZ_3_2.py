ae79b052421846d4c39e4c88fd9c91f92dbfc665
AmigoFox <prostoggwpandre@gmail.com>
2024-10-19 17:23:30 +0300

    Завершены пз 2 и 3

--- PZ_3/PZ_3_2.py
+++ PZ_3/PZ_3_2.py
@@ -3,6 +3,6 @@ def exercise_one():
  num1=int(input("input number 1 \n"))
  num2=int(input("input number 2 \n"))
  if(num1>num2):
- print("greater number 1:",num1)
+  print("greater number 1:",num1)
  else:
- print("greater number 2:",num2)
+  print("greater number 2:",num2)
