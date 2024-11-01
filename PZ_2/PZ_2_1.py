8e5b1c3d0f70dfdb6b27d1e1d6d6baf574451b8e
AmigoFox <prostoggwpandre@gmail.com>
2024-11-01 15:43:57 +0300

    Update PZ_2_1.py

--- PZ_2/PZ_2_1.py
+++ PZ_2/PZ_2_1.py
@@ -1,8 +1,9 @@
-# Вариант20. Дано 3-х значное число. Вывести число полученное при 
-#перестановке цифр исходного числа
-number = int(input("inpyt num "))
-hundreds = number // 100
-tens = (number % 100) // 10
-units = number % 10
-new_number = tens * 100 + hundreds * 10 + units
-print("Number with rearranged hundreds digits and base:", new_number)
+try:
+  number = int(input("Введи число "))
+  hundreds = number // 100
+  tens = (number % 100) // 10
+  units = number % 10
+  new_number = tens * 100 + hundreds * 10 + units
+  print("Измененное число:", new_number)
+except ValueError:
+  print("Нельзя вводить текст! Введите целое число.")
