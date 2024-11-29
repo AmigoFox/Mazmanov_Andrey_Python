e86868c52e147d680c270f42006a5bf0774d905b
AmigoFox <prostoggwpandre@gmail.com>
2024-11-29 12:52:18 +0300

    fin

--- PZ5/PZ_5_1.py
+++ PZ5/PZ_5_1.py
@@ -1,1 +1,8 @@
 #создать def которая напечатает любые 40 символов
+import random
+# 33, 255 ищи в диапазане
+def randon_letter():
+    for i in range(1,40+1):
+        print(i, chr(random.randint(33,255)))
+
+randon_letter()
