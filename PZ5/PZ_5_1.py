f0b45f3ea1ab7f4686d8b7aabb3bb6d48292c99c
AmigoFox <prostoggwpandre@gmail.com>
2024-11-30 16:26:12 +0300

    fin

--- PZ5/PZ_5_1.py
+++ PZ5/PZ_5_1.py
@@ -1,8 +1,12 @@
 #создать def которая напечатает любые 40 символов
 import random
-# 33, 255 ищи в диапазане
+# 33-255 ищи в диапазане
 def randon_letter():
-    for i in range(1,40+1):
-        print(i, chr(random.randint(33,255)))
+    letters = []
+    for i in range(1, 41):
+        letters.append((i, chr(random.randint(33, 255))))
+    return letters
 
-randon_letter()
+all_letters = randon_letter()
+for i, letter in all_letters:
+    print(f"{i}: {letter}")
--- PZ5/PZ_5_2.py
+++ PZ5/PZ_5_2.py
@@ -2,8 +2,22 @@
 Составить функцию, которая будет находить на сколько квадратов
 можно разрезать данный прямоугольник,
  если от него каждый раз отрезать квадрат наибольшей площади.'''
+import numbers
 
-def rectangle():
-    A = int(input('веди сторону прямоугольника А'))
-    B = int(input('веди сторону прямоугольника B'))
-    print()
+def rec(A, B):
+    print(f"Сторона A: {A}")
+    print(f"Сторона B: {B}")
+
+def get_rectangle_sides():
+    try:
+        A = int(input('Введите сторону прямоугольника A (натуральное число): '))
+        B = int(input('Введите сторону прямоугольника B (натуральное число): '))
+        if not (isinstance(A, numbers.Integral) and A > 0 and isinstance(B, numbers.Integral) and B > 0):
+            raise ValueError("Введите натуральные числа, больше 0")
+        return A, B
+    except ValueError as e:
+        print(f"Ошибка: {e}")
+        return get_rectangle_sides()  # Recursive call for invalid input
+
+A, B = get_rectangle_sides()
+rec(A, B)
