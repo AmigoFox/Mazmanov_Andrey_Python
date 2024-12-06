a44176e3903a16ca6c3eebf107f1c4ea67e23ea7
AmigoFox <prostoggwpandre@gmail.com>
2024-12-06 19:00:36 +0300

    все готово.

--- PZ5/PZ_5_2.py
+++ PZ5/PZ_5_2.py
@@ -3,9 +3,6 @@
 можно разрезать данный прямоугольник,
  если от него каждый раз отрезать квадрат наибольшей площади.'''
 def count_squares(a, b):
-    if not (isinstance(a, int) and isinstance(b, int) and a > 0 and b > 0):
-        return -1
-
     count = 0
     while a > 0 and b > 0:
         side = min(a, b)
@@ -14,17 +11,12 @@ def count_squares(a, b):
         b = b - side if b >= a else b
     return count
 
-while True:
-    try:
-        a = int(input("Введите длину стороны A: "))
-        b = int(input("Введите длину стороны B: "))
-        if a <= 0 or b <= 0:
-            raise ValueError("Длины сторон должны быть положительными числами.")
-        result = count_squares(a, b)
-        if result == -1:
-            print("Ошибка: Введите положительные целые числа.")
-        else:
-            print(f"Можно вырезать {result} квадратов.")
-            break
-    except ValueError:
-        print("Ошибка: ")
+try:
+    a = int(input("Введите длину стороны A: "))
+    b = int(input("Введите длину стороны B: "))
+    if a <= 0 or b <= 0:
+        raise ValueError("Ошибка: Только целые, положительные числа!")
+    result = count_squares(a, b)
+    print(f"Можно вырезать {result} квадратов.")
+except ValueError as ve:
+    print(ve)
